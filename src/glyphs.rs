fede3c7ae626262e6c9433cdaf58beeecc5cffe4
Fernando Vega <pytness@gmail.com>
2026-03-31 02:10:11 +0200

    refactor: extract macros to separate module

--- src/glyphs.rs
+++ src/glyphs.rs
@@ -2,72 +2,9 @@ use freetype::{Library, face::LoadFlag};
 use glow::HasContext;
 use rustybuzz::{Face as RbFace, UnicodeBuffer};
 use std::collections::HashMap;
-macro_rules! assets_path {
-    ($name: literal) => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/assets/", $name)
-    };
-}
 
-macro_rules! include_shader {
-    ($gl: ident, $name: literal ) => {{
-        let gl = &$gl;
-        let program = gl.create_program().expect("Cannot create program");
+use crate::macros::macs::include_shader;
 
-        let vs_source = include_str!(concat!(
-            assets_path!("shaders/"),
-            $name,
-            "/",
-            $name,
-            ".vert"
-        ));
-
-        let fs_source = include_str!(concat!(
-            assets_path!("shaders/"),
-            $name,
-            "/",
-            $name,
-            ".frag"
-        ));
-
-        let vs = gl
-            .create_shader(glow::VERTEX_SHADER)
-            .expect("Cannot create vertex shader");
-
-        gl.shader_source(vs, vs_source);
-        gl.compile_shader(vs);
-
-        if !gl.get_shader_compile_status(vs) {
-            panic!(
-                "Vertex shader compilation failed:\n{}",
-                gl.get_shader_info_log(vs)
-            );
-        }
-
-        let fs = gl
-            .create_shader(glow::FRAGMENT_SHADER)
-            .expect("Cannot create fragment shader");
-        gl.shader_source(fs, fs_source);
-        gl.compile_shader(fs);
-        if !gl.get_shader_compile_status(fs) {
-            panic!(
-                "Fragment shader compilation failed:\n{}",
-                gl.get_shader_info_log(fs)
-            );
-        }
-
-        gl.attach_shader(program, vs);
-        gl.attach_shader(program, fs);
-        gl.link_program(program);
-        if !gl.get_program_link_status(program) {
-            panic!("Program link failed:\n{}", gl.get_program_info_log(program));
-        }
-
-        gl.delete_shader(vs);
-        gl.delete_shader(fs);
-
-        program
-    }};
-}
 struct GlyphId(u32);
 
 struct ShapedGlyph {
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod glyphs;
+mod macros;
 
 use freetype::face::LoadFlag;
 use glfw::{Action, Context as _, Key, WindowEvent, WindowHint, WindowMode};
@@ -6,77 +7,7 @@ use glow::{HasContext, NativeTexture};
 use rustybuzz::{Face, UnicodeBuffer};
 
 use self::glyphs::TextRenderer;
-
-macro_rules! assets_path {
-    ($name: literal) => {
-        concat!(env!("CARGO_MANIFEST_DIR"), "/assets/", $name)
-    };
-}
-
-macro_rules! include_shader {
-    ($gl: ident, $name: literal ) => {{
-        let gl = &$gl;
-        let program = gl.create_program().expect("Cannot create program");
-
-        let vs_source = include_str!(concat!(
-            assets_path!("shaders/"),
-            $name,
-            "/",
-            $name,
-            ".vert"
-        ));
-
-        let fs_source = include_str!(concat!(
-            assets_path!("shaders/"),
-            $name,
-            "/",
-            $name,
-            ".frag"
-        ));
-
-        let vs = gl
-            .create_shader(glow::VERTEX_SHADER)
-            .expect("Cannot create vertex shader");
-
-        gl.shader_source(vs, vs_source);
-        gl.compile_shader(vs);
-
-        if !gl.get_shader_compile_status(vs) {
-            panic!(
-                "Vertex shader compilation failed:\n{}",
-                gl.get_shader_info_log(vs)
-            );
-        }
-
-        let fs = gl
-            .create_shader(glow::FRAGMENT_SHADER)
-            .expect("Cannot create fragment shader");
-        gl.shader_source(fs, fs_source);
-        gl.compile_shader(fs);
-        if !gl.get_shader_compile_status(fs) {
-            panic!(
-                "Fragment shader compilation failed:\n{}",
-                gl.get_shader_info_log(fs)
-            );
-        }
-
-        gl.attach_shader(program, vs);
-        gl.attach_shader(program, fs);
-        gl.link_program(program);
-        if !gl.get_program_link_status(program) {
-            panic!("Program link failed:\n{}", gl.get_program_info_log(program));
-        }
-
-        gl.delete_shader(vs);
-        gl.delete_shader(fs);
-
-        program
-    }};
-}
-
-macro_rules! include_font {
-    ($name: literal) => {{ include_bytes!(concat!(assets_path!("fonts/"), $name)) }};
-}
+use self::macros::macs::{include_font, include_shader};
 
 fn main() {
     // Initialize GLFW
